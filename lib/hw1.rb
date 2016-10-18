0e042be9fef9d618a7fccf97fda1918ccd57ea5e
clarisatomasini <clarisatomasini@gmail.com>
2016-10-18 19:14:01 -0400

    Finalizado

--- lib/hw1.rb
+++ lib/hw1.rb
@@ -6,22 +6,40 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 # http://ruby-doc.org/core-2.2.0/Regexp.html
 def validate_email?(email)
   # YOUR CODE HERE
+  valido = email =~ VALID_EMAIL_REGEX
+  !valido.nil? ? true : false
 end
 
 def is_palindrome?(phrase)
   # YOUR CODE HERE
+  return false if phrase.empty?
+  arr_phrase = phrase.downcase.scan(/\w/)
+  arr_phrase == arr_phrase.reverse
 end
 
 # Part 2
 def max_sum_of_2(arr1, arr2)
   # YOUR CODE HERE
+  sum1 = 0
+  arr1.each{|x| sum1 += x}
+  sum2 = 0
+  arr2.each{|x| sum2 += x}
+  sum2 if sum2 > sum1
+  sum1 if sum1 > sum2
+  if sum1 == sum2
+    puts "sums are equal"
+    return 0
+  end
 end
 
 def word_frequency(words)
   # YOUR CODE HERE
-end
-
-# Part 3
-class FileAnalizer
-  # YOUR CODE HERE
+  freq = Hash.new(0)
+  words.each{|x| freq[x] += 1}
+  freq.sort_by {|x,y| y}
+  freq.reverse!
+  freq.each{|x,y| 
+    return x 
+    break
+    }
 end
---
+++ lib/ruby_intro.rb
@@ -0,0 +1,21 @@
+# Part 3
+class FileAnalizer
+  # YOUR CODE HERE
+  private
+  @file_name
+  
+  private
+  @total_lines
+  def initialize(file, lines = 0)
+    @file_name = file_name
+    @total_lines = lines
+  end
+  
+  public
+  def calculate_total_lines
+  end
+  
+  public 
+  def max_word_line
+  end
+end
